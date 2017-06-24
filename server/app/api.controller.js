6ebfbc9c8b9c1b7e5601412ee77b1c0867db9caf
mjt <thapa.manish16@gmail.com>
2017-06-24 14:56:05 +0545

    taking strings from strings.json file

--- server/app/api.controller.js
+++ server/app/api.controller.js
@@ -4,14 +4,25 @@ var request = require('request');
 
 var config = require('../config.json');
 
+var textCollection = require("../../data/type-content/strings.json");
+
+var length = textCollection.length;
 
 router.get('/getstring/:id', function (req,res) {
 
 
-	var id = req.params.id;
+	var id = parseInt(req.params.id);
 
-	var text = "सानो छ खेत सानो छ बारी सानै छ जहान, नगरी काम पुग्दैन खान साझ र बिहान, बिहानपख झुल्किनछ घाम देउराली पाखामा, आसरे गीत घन्किनछ अनि सुरिलो भाकामा";
-	res.json({text : text});
+	console.log(req.params.id,id);
+
+	if (id =>length) {
+		id = 0;
+	}
+
+
+	var data = textCollection[id];
+
+	res.json({data : data});
 });
 
 module.exports = router;
