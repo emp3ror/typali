1c8d2570b92ca3aca5c976c4a8d6cc596705bcd2
mjt <thapa.manish16@gmail.com>
2017-06-25 15:34:40 +0545

    dist minor

--- client/services/datafetch.service.js
+++ client/services/datafetch.service.js
@@ -10,8 +10,8 @@
 	function DatafetchService ($http) {
 		var service = this;
 		
-		// var url = "https://typali.herokuapp.com/api/";
-		var url = "http://localhost:8080/api/";
+		var url = "https://typali.herokuapp.com/api/";
+		// var url = "http://localhost:8080/api/";
 
 		/*get string*/
 		service.getString = function (params) {
